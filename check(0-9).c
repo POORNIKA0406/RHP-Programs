1c44c52bc67578a406104f12e02676b175787e80
POORNIKA0406 <poornika0604@gmail.com>
2026-06-06 03:08:25 +0530

    Update check(0-9).c

--- check(0-9).c
+++ check(0-9).c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+  char str[1000001];
+  scanf("%s",str);
+  int flag=0;
+  for(int i=0;str[i];i++){
+    if(str[i] >= '0' && str[i] <='9'){
+      flag = flag | (1 << (str[i]-'0'));
+    }
+  }
+  if (flag == (1<<10) -1 ) printf("Yes");
+  else printf("No");
+  return 0;
+}
