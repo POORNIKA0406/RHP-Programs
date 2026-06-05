7bf554345f89e4163f376675e075df05e3d89f1e
POORNIKA0406 <poornika0604@gmail.com>
2026-06-06 02:58:33 +0530

    Update PanagramCheacking(A-Z).c

--- PanagramCheacking(A-Z).c
+++ PanagramCheacking(A-Z).c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+  char str[100001];
+  scanf("%s" , str);
+  int flag =0;
+  for(int i=0;str[i];i++){
+    if(str[i]>='A' && str[i]<='Z'){
+      flag=flag | (1<< (str[i]-'A'));
+    }
+  }
+  if(flag == (1<<26)-1) printf("Yes");
+  else printf("No");
+  return 0;
+}
