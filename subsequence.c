ed04cf3341c86eb1b693ca2859e101437ca0f7b0
POORNIKA0406 <poornika0604@gmail.com>
2026-06-06 03:15:31 +0530

    Update subsequence.c

--- subsequence.c
+++ subsequence.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+#include <string.h>
+int main(){
+  char str[1001];
+  scanf("%s" , str);
+  int n = strlen(str);
+  for(int i=0;i<(1<<n);i++){
+    for(int j=0;j<n;j++){
+      if(i & (1<<j)) printf("%c" , str[j]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
