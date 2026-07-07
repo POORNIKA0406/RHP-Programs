651734d0f105703c95a0e9136ab926c2735f70bc
POORNIKA0406 <poornika0604@gmail.com>
2026-07-07 10:30:43 +0530

    Update sushi pair matching.java

--- sushi pair matching.java
+++ sushi pair matching.java
@@ -1,1 +1,29 @@
-
+import java.util.*;
+public class SushiPairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); 
+        int M = sc.nextInt(); 
+        int[] A = new int[N]; 
+        int[] B = new int[M];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        for (int j = 0; j < M; j++) {
+            B[j] = sc.nextInt();
+        }
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int i = 0, j = 0, count = 0;
+        while (i < N && j < M) {
+            if (B[j] <= 2 * A[i]) {
+                count++;
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+        System.out.println(count);
+    }
+}
